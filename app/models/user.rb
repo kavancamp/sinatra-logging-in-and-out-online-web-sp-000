50697efb4da0bb5735ef56fdcdd3b3e44e9ac023
Keenah Vancampenhout <kvancamp@mail.com>
2021-02-21 20:37:14 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-
+  def change
+     create_table :users do |t|
+       t.string :username
+       t.string :password
+       t.decimal :balance
+     end
+   end
 end
